56264b0933ee8a04a3ffca0bdc48661df95c6808
OleksiiT-in6k <OleksiiT@softwareplanetgroup.com>
2016-07-08 15:10:56 +0300

    Return an empty sequence on an empty list

--- test/SequenceTest.java
+++ test/SequenceTest.java
@@ -1,17 +1,26 @@
 /**
  * Created by employee on 7/8/16.
  */
+
 import com.interlink.FindingSequence;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.interlink.FindingSequence.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.*;
+
 public class SequenceTest {
     @org.junit.Test
+    public void nullSequenceTest() throws Exception {
+        assertThat(getSubSequence(null), is(new ArrayList()));
+    }
+
+    @Test
     public void emptySequenceTest() throws Exception {
-        assertThat(FindingSequence.getSubSequence(null),is(new ArrayList()));
+        assertThat(getSubSequence(new ArrayList<>()), is(new ArrayList()));
+
     }
 }
